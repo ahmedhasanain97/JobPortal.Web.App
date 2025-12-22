c3cad4eae3f331bd9ff6d168eb87502a076cb663
Ahmed Hasanain <ahmedhasanain97@gmail.com>
2025-12-22 02:43:43 +0200

    Add project files.

---
+++ JobPortal.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            // Register application services here
+            return services;
+        }
+    }
+}
---
+++ JobPortal.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+        {
+            // Register infrastrurcture services here
+            return services;
+        }
+    }
+}
