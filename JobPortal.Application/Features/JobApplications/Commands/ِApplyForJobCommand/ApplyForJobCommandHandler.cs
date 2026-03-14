620aab26dec4dbac015cd3dac2be9b62377fe143
Ahmed Hasanain <ahmedhasanain97@gmail.com>
2026-03-14 19:45:21 +0200

    prevent duplicate application
    closes #42

--- JobPortal.Application/Features/JobApplications/Commands/ِApplyForJobCommand/ApplyForJobCommandHandler.cs
+++ JobPortal.Application/Features/JobApplications/Commands/ِApplyForJobCommand/ApplyForJobCommandHandler.cs
@@ -1,19 +1,39 @@
-﻿namespace JobPortal.Application.Features.JobApplications.Commands._ِApplyForJobCommand
+﻿using JobPortal.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobPortal.Application.Features.JobApplications.Commands._ِApplyForJobCommand
 {
     public class ApplyForJobCommandHandler : IRequestHandler<ApplyForJobCommand, Result>
     {
         private readonly IJobApplicationRepo _jobApplicationRepo;
         private readonly IUnitOfWork _unitOfWork;
-        public ApplyForJobCommandHandler(IJobApplicationRepo jobApplicationRepo, IUnitOfWork unitOfWork)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ApplyForJobCommandHandler(IJobApplicationRepo jobApplicationRepo, IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
         {
             _jobApplicationRepo = jobApplicationRepo;
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
         }
 
         public async Task<Result> Handle(ApplyForJobCommand request, CancellationToken cancellationToken)
         {
+            //check if user exists and is a job seeker
+            var user = await _userManager.FindByIdAsync(request.userId);
+            if (user == null)
+                return Result.Failure(Error.NotFound("User not found"));
+            var isJobSeeker = await _userManager.IsInRoleAsync(user, "JobSeeker");
+            if (!isJobSeeker)
+                return Result.Failure(Error.BadRequest("You can't apply to the job as Employer"));
+
+            //check if job exists and is open for applications and user has not applied before
+            var repo = _unitOfWork.JobRepository;
+            var job = await repo.FindByIdAsync(request.jobId);
+            if (job == null)
+                return Result.Failure(Error.NotFound("Job not found"));
+            if (job.Jobstatus != JobStatus.open)
+                return Result.Failure(Error.BadRequest("This is not open for applications"));
             if (await _jobApplicationRepo.HasUserAppliedToJob(request.jobId, request.userId))
-                return Error.Conflict("You have already applied for this job");
+                return Result.Failure(Error.Conflict("You have already applied for this job"));
 
             await _jobApplicationRepo.CreateApplication(request.jobId, request.userId);
             var result = await _unitOfWork.SaveChangesAsync();
